58ce80eaf3f69aeff88f1d52e682bf4461c34f45
Niamul Islam <ni.jahid75@gmail.com>
2019-04-07 01:15:15 +0600

    Allow fonts upload in wordpress

---
+++ upload-mimes-font.php
@@ -0,0 +1,13 @@
+<?php
+
+//Allow custom font upload in wordpress
+add_filter('upload_mimes', 'ni_filter_mime_types');
+function ni_filter_mime_types($mimes)
+{
+	$mimes['ttf'] = 'font/ttf';
+	$mimes['woff'] = 'font/woff';
+	$mimes['svg'] = 'font/svg';
+	$mimes['eot'] = 'font/eot';
+
+	return $mimes;
+}

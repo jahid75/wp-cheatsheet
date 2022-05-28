3f921e6e3e5faae52ec3098dec393b27b92604bb
Niamul Islam <ni.jahid75@gmail.com>
2022-05-28 17:57:43 +0600

    Create wp-config-constants.php
    Helpful constants for wp-config.php

---
+++ wp-config-constants.php
@@ -0,0 +1,30 @@
+<?php 
+
+/* Disable File Editor. */
+define( 'DISALLOW_FILE_MODS', FALSE );
+
+define( 'DISALLOW_FILE_EDIT', FALSE );
+
+define('AUTOSAVE_INTERVAL', 9999 );
+
+/* Specify maximum number of Revisions. */
+define( 'WP_POST_REVISIONS', '5' );
+
+/* Media Trash. */
+define( 'MEDIA_TRASH', true );
+
+/* Trash Days. */
+define( 'EMPTY_TRASH_DAYS', '10' );
+
+define( 'WP_AUTO_UPDATE_CORE', true );
+
+/* Compress. */
+define( 'ENFORCE_GZIP', true );
+
+define( 'DISABLE_WP_CRON', false );
+
+define( 'WP_TURN_OFF_ADMIN_BAR', false );
+
+define( 'WP_MEMORY_LIMIT', '512M' );
+
+define( 'WP_MAX_MEMORY_LIMIT', '512M' );
